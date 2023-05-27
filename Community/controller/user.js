55f5018cd0e0affe25913678559acc7fb254bf1f
Bhalla <keshavbhalla2015@gmail.com>
2023-05-27 13:59:33 -0700

    nothing much

--- Community/controller/user.js
+++ Community/controller/user.js
@@ -3,8 +3,7 @@ const feed = require('../Models/feed');
 
 exports.getPosts = async (req, res, next) => {
     try {
-    //   const jobs = await feed.find({ User_Id:req.user.id });
-    const jobs = await feed.find({ User_Id:req.user.id });
+    const posts = await feed.find({ User_Id:req.user.id });
       return res.status(200).json({
         posts: posts
       });
